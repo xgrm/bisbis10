4b44e9a659f050b352389bc2d4cc510f083803a4
saarmoseri <xgrm406@gmail.com>
2024-05-09 10:47:24 +0300

    create base controller

---
+++ src/main/java/com/att/tdp/bisbis10/controller/DishController.java
@@ -0,0 +1,9 @@
+package com.att.tdp.bisbis10.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants")
+public class DishController {
+}
